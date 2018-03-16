70068d8a9358fa0295ab8b5f32240275e772659c
Luis Lavaire <llavaire01@gmail.com>
2018-03-15 22:30:24 -0600

    Enable the builds in Travis CI. Simplified the package build process.

--- build.sh
+++ build.sh
@@ -1,2 +1,20 @@
 #! /bin/sh
 
+DEPENDS=$( echo $(sed -e '/^#.*$/d; /^$/d; /^[[:space:]].*$/d' dependencies) | tr ' ' ',')
+
+mkdir -p build
+
+echo "
+Section: misc
+Priority: optional
+Homepage: https://nxos.org
+
+Package: nxos-meta
+Version: 1.1.0
+Maintainer: Luis Lavaire <llavaire01@gmail.com>
+Depends: $DEPENDS
+Architecture: amd64
+Description: Nomad desktop metapackage.
+" > build/package
+
+equivs-build build/package
