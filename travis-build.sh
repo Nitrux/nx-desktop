a02bb568cb8e9c4ce4595007c3f01a52b504718a
Alexis López Zubieta <azubieta90@gmail.com>
2018-04-26 19:33:55 -0500

    Add git commit to the package version

--- travis-build.sh
+++ travis-build.sh
@@ -1,11 +1,13 @@
 #! /bin/sh
 
 apt-get --yes update
-apt-get --yes install wget equivs curl
+apt-get --yes install wget equivs curl git
 
 cd package/
 
 DEPENDS=$( echo $(sed -e '/^#.*$/d; /^$/d; /^[[:space:]].*$/d' dependencies) | tr ' ' ',')
+
+GIT_COMMIT=$(git rev-parse --short HEAD)
 
 echo "
 Section: misc
@@ -13,7 +15,7 @@ Priority: optional
 Homepage: https://nxos.org
 
 Package: nxos-desktop
-Version: 1.1.0
+Version: 1.2.0-${GIT_COMMIT}
 Maintainer: Luis Lavaire <llavaire01@gmail.com>
 Depends: $DEPENDS
 Architecture: amd64
