4734e90e70daa0043064ad9c21ec623f499080ae
UriHerrera <hello@uriherrera.com>
2018-10-31 00:23:47 -0600

    update list, once again

--- travis-build.sh
+++ travis-build.sh
@@ -15,7 +15,7 @@ Priority: optional
 Homepage: https://nxos.org
 
 Package: nomad-desktop
-Version: 1.4.5.2-${GIT_COMMIT}
+Version: 1.4.5.3-${GIT_COMMIT}
 Maintainer: Luis Lavaire <llavaire01@gmail.com>
 Depends: $DEPENDS
 Architecture: amd64
