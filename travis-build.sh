0f8b6e51bce63e22ee52bd0f7abab49a09bbe60c
Uri Herrera <hello@uriherrera.com>
2020-08-15 20:13:57 -0500

    update travis files

--- travis-build.sh
+++ travis-build.sh
@@ -3,22 +3,18 @@
 apt-get --yes update
 apt-get --yes install wget equivs curl git
 
-cd package/
+deps=$(sed -e '/^#.*$/d; /^$/d; /^\s*$/d' dependencies | paste -sd ,)
+git_commit=$(git rev-parse --short HEAD)
 
-DEPENDS=$( echo $(sed -e '/^#.*$/d; /^$/d; /^[[:space:]].*$/d' dependencies) | tr ' ' ',')
-
-GIT_COMMIT=$(git rev-parse --short HEAD)
-
-echo "
-Section: misc
-Priority: optional
-Homepage: https://nxos.org
-Package: nx-desktop-legacy
-Version: 1.4.9.48-${GIT_COMMIT}
-Maintainer: Uri Herrera <uri_herrera@nxos.org>
-Depends: $DEPENDS
-Architecture: amd64
-Description: NX Desktop metapackage for Nitrux.
-" > configuration
+> configuration printf "%s\n" \
+	"Section: misc" \
+	"Priority: optional" \
+	"Homepage: https://nxos.org" \
+	"Package: nx-desktop-legacy" \
+	"Version: 1.4.9.48-${GIT_COMMIT}" \
+	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
+	"Depends: $deps" \
+	"Architecture: amd64" \
+	"Description: Hardware drivers metapackage for Nitrux."
 
 equivs-build configuration
