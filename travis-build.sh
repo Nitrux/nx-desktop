076b95ec2de1e51543b678c18785f684c641503b
Uri Herrera <hello@uriherrera.com>
2020-08-25 04:29:39 -0500

    add conflicts

--- travis-build.sh
+++ travis-build.sh
@@ -11,9 +11,10 @@ git_commit=$(git rev-parse --short HEAD)
 	"Priority: optional" \
 	"Homepage: https://nxos.org" \
 	"Package: nx-desktop-legacy" \
-	"Version: 1.5.1-$git_commit" \
+	"Version: 1.5.2-$git_commit" \
 	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
 	"Depends: $deps" \
+	"Conflicts: nx-desktop" \
 	"Architecture: amd64" \
 	"Description: NX Desktop metapackage for Nitrux."
 
