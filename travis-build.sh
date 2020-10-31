83cffa0d973ab03a98a3d2b8a15c2ac0a9e35421
Uri Herrera <hello@uriherrera.com>
2020-10-30 20:28:06 -0600

    add deps

--- travis-build.sh
+++ travis-build.sh
@@ -11,7 +11,7 @@ git_commit=$(git rev-parse --short HEAD)
 	"Priority: optional" \
 	"Homepage: https://nxos.org" \
 	"Package: nx-desktop-legacy" \
-	"Version: 1.6.1-$git_commit" \
+	"Version: 1.6.2-$git_commit" \
 	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
 	"Depends: $deps" \
 	"Conflicts: nx-desktop" \
