4a49898ba92d971910f7ef62723f9c17c8446282
UriHerrera <hello@uriherrera.com>
2021-12-25 03:41:57 -0600

    rm deps

--- travis-build.sh
+++ travis-build.sh
@@ -11,7 +11,7 @@ git_commit=$(git rev-parse --short HEAD)
 	"Priority: optional" \
 	"Homepage: https://nxos.org" \
 	"Package: nx-desktop-legacy" \
-	"Version: 1.8.6-$git_commit" \
+	"Version: 1.8.7-$git_commit" \
 	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
 	"Depends: $deps" \
 	"Conflicts: nx-desktop, synaptic" \
