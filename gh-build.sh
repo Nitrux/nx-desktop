2465f61d0338857aad3275e0caedcfc69d906388
Uri Herrera <hello@uriherrera.com>
2025-06-10 16:26:31 -0600

    update ci scripts
    - remove wayland branch and merge contents with this package

--- gh-build.sh
+++ gh-build.sh
@@ -1,23 +1,44 @@
-#! /bin/sh
+#!/usr/bin/env bash
 
-apt -qq update
-apt -qq -yy install equivs curl git
+#############################################################################################################################################################################
+#   The license used for this file and its contents is: BSD-3-Clause                                                                                                        #
+#                                                                                                                                                                           #
+#   Copyright <2025> <Uri Herrera <uri_herrera@nxos.org>>                                                                                                                   #
+#                                                                                                                                                                           #
+#   Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:                          #
+#                                                                                                                                                                           #
+#    1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.                                        #
+#                                                                                                                                                                           #
+#    2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer                                      #
+#       in the documentation and/or other materials provided with the distribution.                                                                                         #
+#                                                                                                                                                                           #
+#    3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software                    #
+#       without specific prior written permission.                                                                                                                          #
+#                                                                                                                                                                           #
+#    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,                      #
+#    THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS                  #
+#    BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE                 #
+#    GOODS OR SERVICES; LOSS OF USE, DATA,   OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,                      #
+#    STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.   #
+#############################################################################################################################################################################
 
-git config --global --add safe.directory /home/travis/build/Nitrux/nx-desktop
+
+# -- Build package.
 
 deps=$(sed -e '/^#.*$/d; /^$/d; /^\s*$/d' package/dependencies | paste -sd ,)
-git_commit=$(git rev-parse --short HEAD)
+
+git_commit=$(git rev-parse --short HEAD 2>/dev/null)
+[ -z "$git_commit" ] && git_commit="0"
 
 > configuration printf "%s\n" \
 	"Section: misc" \
-	"Priority: optional" \
+	"Priority: required" \
 	"Homepage: https://nxos.org" \
-	"Package: nx-desktop-legacy" \
-	"Version: 3.3.8-$git_commit" \
+	"Package: nx-desktop" \
+	"Version: 3.3.9-$git_commit" \
 	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
 	"Depends: $deps" \
-	"Conflicts: nx-desktop, synaptic" \
-	"Architecture: amd64" \
+	"Architecture: all" \
 	"Description: NX Desktop metapackage for Nitrux."
 
 equivs-build configuration
