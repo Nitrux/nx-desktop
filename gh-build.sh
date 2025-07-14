b492ef461617663df8f72fadd2f1b734c9e2ac7c
UriHerrera <hello@uriherrera.com>
2025-07-13 20:08:45 -0600

    ....

--- gh-build.sh
+++ gh-build.sh
@@ -35,7 +35,7 @@ git_commit=$(git rev-parse --short HEAD 2>/dev/null)
 	"Priority: required" \
 	"Homepage: https://nxos.org" \
 	"Package: nitrux-desktop" \
-	"Version: 3.4.1-$git_commit" \
+	"Version: 3.4.2-$git_commit" \
 	"Maintainer: Uri Herrera <uri_herrera@nxos.org>" \
 	"Depends: $deps" \
 	"Architecture: all" \
